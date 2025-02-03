a3989b85bd8d09908511d59067002fdb094bf739
Thomas Tomow <toto_san@live.com>
2025-02-03 13:32:03 +0100

    added semantic kernel

---
+++ tut-aiagents/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using OpenAI.VectorStores;
+
+namespace tut_aiagents
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DotNetEnv.Env.Load();
+            // Create a new instance of the OpenAI connector
+            var azureOpenAIConnector = CreateKernelWithChatCompletion();
+
+        }
+
+        static Kernel CreateKernelWithChatCompletion()
+        {
+            var builder = Kernel.CreateBuilder();
+
+            AddChatCompletionToKernel(builder);
+
+            return builder.Build();
+        }
+
+        static void AddChatCompletionToKernel(IKernelBuilder builder)
+        {
+            try
+            {
+                builder.AddAzureOpenAIChatCompletion(
+                    Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL_ID"),
+                    Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT"),
+                    Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY"));
+            }
+            catch (System.Exception)
+            {
+                throw new Exception("Please set the environment variables AZURE_OPENAI_MODEL_ID, AZURE_OPENAI_ENDPOINT, and AZURE_OPENAI_API_KEY");
+            }
+        }
+    }
+}
